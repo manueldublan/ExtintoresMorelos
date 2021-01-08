2a89a3e5f33bd072671419841cd548fdc8378844
manuel <joseflor@mx1.ibm.com>
2021-01-08 12:30:00 -0600

    last change december 2020

--- index.php
+++ index.php
@@ -11,8 +11,9 @@
 
     </head>
     <header>
-        <!-- <?php include 'includes\header.php'; ?> -->
-        <h1 style="color:white"> Extintores Morelos </h1>
+         <!-- <?php include 'img\header.png'; ?> -->
+        <!-- <h1 style="color:white"> Extintores Morelos </h1> -->
+        <img class=extrs_mor alt="extintores morelos" src=img/header.png >
     </header>
     <body background=img\FireExtinguishers.jpg>
         <section id="screen1">
@@ -41,33 +42,46 @@
         </div></section>
 
 
-        <section class=articulos>
-        <article class=articulo> 
+        <section class=articulo>
+        
         <h3><i class="fa fa-fire-extinguisher fa-3x"></i>EXTINTORES MORELOS</h3>
         <p> Le brindará a usted la asesoría profesional, para que de manera adecuada, cubramos sus necesidades en materia de seguridad y equipo contra incendio, con la plena seguridad de que todos nuestros productos y trabajos se elabroran bajo los más estrictos estándares de calidad, siempre rigiéndonos por las Normas Oficiales Mexicanas y controles de calidad internacionales. </p><br></article>
         
          <!--div*3>{span!} -->  
-         <article class=articulo>
          <h3><i class="fa fa-fire  fa-3x"></i>Misión</h3>
          <p>Fabricar y comercializar equipo contra incendio de la más alta calidad, asumiendo el compromiso de brindarle una solución integral a sus requerimientos, con el respaldo de una marca líder en el mercado, con precios competitivos y la garantía de suministro y servicio oportunos.</p></article><br>
 
-        <article class=articulo> 
         <h3><i class="fa fa-fire  fa-3x"></i>VISIÓN</h3>
         <p>Ser reconocidos como una empresa de clase mundial, líder en la fabricación y comercialización de equipo contra Incendio, cuya marca sea una referencia de calidad y confianza. Ofreciéndole a nuestros clientes las mejores alternativas y soluciones integrales en materia de seguridad, mediante un servicio profesional, ético y responsable.</p></article><br>
             
-        <article class=articulo>
         <h3><i class="fa fa-fire  fa-3x"></i>Recarga y Mantenimiento de Extintores</h3>
          <p>El mantenimiento es una verificación completa del extinguidor. Tiene el fin de dar la máxima seguridad de que el extinguidor funcionara efectiva y seguramente. Incluye un examen completo y cualquier reparación o repuesto que necesite. Los extinguidores deben estar sujetos a mantenimiento anualmente, cuando sea indicado específicamente por una inspección bimestral o en el momento de la prueba hidrostática.<p></article><br>
 
-        <article class=articulo>
         <h3><i class="fa fa-fire  fa-3x"></i>Mantenimiento</h3> 
         <p>Los procedimientos de mantenimiento deben incluir un examen minucioso de los tres elementos básicos de un extinguidor:</p>
-            <ul id=circle>
+        <!-- <ul style="list-style-position: inside;">
                 <li>Partes Mecánicas</li>
                 <li>Agente Extintor</li>
                 <li>Medios Expelentes</li>
-            </ul>
-        <p>Cada extinguidor debe tener una etiqueta o rotulo seguramente adherida y que indique el mes y año en que se ejecutó el mantenimiento y debe indicar la persona o empresa que realizo el servicio.</p></article>
+            </ul> -->
+        <dl>
+            <dd>Partes Mecánicas</dd>
+            <dd>Agente Extintor</dd>
+            <dd>Medios Expelentes</dd>
+        </dl>
+        <p>Cada extinguidor debe tener una etiqueta o rotulo seguramente adherida y que indique el mes y año en que se ejecutó el mantenimiento y debe indicar la persona o empresa que realizo el servicio.</p><br>
+    
+        <p> La recarga: Es el reemplazo del agente de extinción y también del expelente para ciertos tipos de extinguidores.
+Todos los extinguidores de tipo recargable deben ser recargados después de ser utilizados o cuando lo indique una inspección. Cuando se lleva a cabo la recarga, se deben seguir las recomendaciones de los fabricantes.</p><br>
+
+<h4><p><b>Por este medio quiero darle a conocer los beneficios hacia su empresa:</b></p></h4><br>
+<ul class="policies">
+    <li>Póliza de Garantía: Esta consiste en que nos hacernos responsables del equipo recargado durante un año.</li></br>
+    <li>Carta Responsiva: Consiste en respaldar a la póliza de garantía.</li></br>
+    <li>Constancia de Certificación: Es el dictamen de cumplimiento de acuerdo con la Norma Oficial Mexicana NOM-154-SCFI-2005. En la evaluación y acreditación de “equipos contra incendio-extintores-servicio de mantenimiento y recarga”.</li>
+</ul><br>
+
+<p>Cabe mencionar que somos una empresa seria y responsable, además de que contamos con una amplia experiencia en nuestros servicios, avalados por el Dictamen de Cumplimiento VENCE, Norma Oficial Mexicana NOM 154 SCFI-2005.</p></article>
 
         
     </section>
